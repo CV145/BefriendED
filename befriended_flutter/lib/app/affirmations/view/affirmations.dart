6e8cd73456837641219f7f2919d434911d6e4268
Carlos <carlosvaleriano45@yahoo.com>
2022-09-28 17:26:35 -0500

    Added scaffold and app bar to affirmations page

--- befriended_flutter/lib/app/affirmations/view/affirmations.dart
+++ befriended_flutter/lib/app/affirmations/view/affirmations.dart
@@ -2,57 +2,53 @@ import 'package:befriended_flutter/services/local_notification_service.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
-class AffirmationsPage extends StatefulWidget
-{
-  const AffirmationsPage({Key? key}): super(key: key);
+class AffirmationsPage extends StatefulWidget {
+  const AffirmationsPage({Key? key}) : super(key: key);
 
   @override
   State<AffirmationsPage> createState() => _AffirmationsState();
 }
 
-class _AffirmationsState extends State<AffirmationsPage>
-{
+class _AffirmationsState extends State<AffirmationsPage> {
   late final LocalNotificationService service;
 
   @override
-  void initState()
-  {
+  void initState() {
     service = LocalNotificationService();
     service.initialize();
     super.initState();
   }
 
   @override
-  Widget build(BuildContext context)
-  {
-    return Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      children:
-        [
-          ElevatedButton
-          (
-              onPressed: () async
-              {
-                await service.showNotification(id: 0,
-                    title: 'Notification Title',
-                    body: 'Affirmation quote goes here');
-              },
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Scheduled Affirmations", style: TextStyle(fontSize: 15)),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () async {
+              await service.showNotification(
+                  id: 0,
+                  title: 'Notification Title',
+                  body: 'Affirmation quote goes here');
+            },
             child: const Text("Show quote notification"),
           ),
-          ElevatedButton
-          (
-            onPressed: () async
-            {
-              await service.showScheduledNotification
-              (   id: 0,
-                  title: 'Notification Title',
-                  body: 'Affirmation quote goes here',
-                  seconds: 4,
+          ElevatedButton(
+            onPressed: () async {
+              await service.showScheduledNotification(
+                id: 0,
+                title: 'Notification Title',
+                body: 'Affirmation quote goes here',
+                seconds: 4,
               );
             },
             child: const Text("Show delayed notification"),
           ),
         ],
+      ),
     );
   }
 }
