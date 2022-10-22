d984d62020411e3f43f73ee4b9d71d24d8a74e39
Carlos <carlosvaleriano45@yahoo.com>
2022-10-21 19:25:01 -0500

    Sample chat topics added

---
+++ befriended_flutter/lib/app/user_profile/tags_pool.dart
@@ -0,0 +1,20 @@
+import 'dart:core';
+
+
+/*
+'Tags' are what the user can display on their profile to share with others
+the kinds of symptoms and topics they would like to talk about. These are
+pre-defined so users can just choose like 3-5 of them from this pool
+ */
+
+class Tags
+{
+  //https://breathelifehealingcenters.com/12-types-eating-disorders-explained/
+  final List<String> _eatingDisorderTopics = [
+    'Anorexia', 'Binge Eating', 'Bulimia', 'Muscle Dysmorphia',
+    'General Eating Disorder', 'Compulsive Eating', 'Chronic Fatigue',
+    'Diabetes', 'Diabulimia', 'Orthorexia', 'Selective Eating', 'Drunkorexia',
+    'Pregorexia'
+  ];
+}
+
