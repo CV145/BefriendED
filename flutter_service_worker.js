e92092d1d9e63252f97865d0472e6467e644499b
Carlos <carlosvaleriano45@yahoo.com>
2022-11-23 17:12:59 -0600

    Built web

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -47,8 +47,8 @@ const RESOURCES = {
 "icons/favicon.png": "7a26ea9d4e61122fc023b84e5acdb148",
 "icons/Icon-192.png": "12d05db1c0b26d429b54e3f8c946a496",
 "icons/Icon-512.png": "aabba36693aba0ec8d954adf297d3125",
-"index.html": "bcd790ffed2f866d3110654855cb9eab",
-"/": "bcd790ffed2f866d3110654855cb9eab",
+"index.html": "6e7edffabad98571446cb8ba53ae6b8f",
+"/": "6e7edffabad98571446cb8ba53ae6b8f",
 "main.dart.js": "74f1e058828f633eee63d60e01c6ef1e",
 "manifest.json": "bf141e617bbfbcc135ad757050953bbb",
 "version.json": "04200bf8c223124481967b6763fe7911"
